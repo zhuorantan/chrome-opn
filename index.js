ffc4add0b48dee4d60230319b425b7c1b035c59a
谭卓然 <zhuoran.zr@alibaba-inc.com>
2018-11-13 16:02:35 +0800

    Implement chrome open

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 'use strict';
 const opn = require('opn');
+
+function getChromeAppName() {
+  switch (process.platform) {
+    case "darwin":
+      return "google chrome";
+    case "win32":
+      return "chrome";
+    default:
+      return "google-chrome";
+  }
+}
+
+module.exports = (url, args, wait) => {
+  const appName = getChromeAppName();
+  const app = args ? [appName, ...args] : appName;
+
+  return opn(url, {app, wait})
+}
